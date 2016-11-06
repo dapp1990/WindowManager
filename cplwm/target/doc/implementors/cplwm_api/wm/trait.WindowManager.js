227bb670119b3da06457f5b419d88db5079e854a
Daniel Andrés Pérez Pérez <dapp.1990@gmail.com>
2016-11-06 16:45:15 +0100

    Initial commit

---
+++ cplwm/target/doc/implementors/cplwm_api/wm/trait.WindowManager.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cplwm_assignment"] = ["impl <a class='trait' href='cplwm_api/wm/trait.WindowManager.html' title='cplwm_api::wm::WindowManager'>WindowManager</a> for <a class='struct' href='cplwm_assignment/a_fullscreen_wm/struct.FullscreenWM.html' title='cplwm_assignment::a_fullscreen_wm::FullscreenWM'>FullscreenWM</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Window Manager Project
+This repo contains Comparative Programming Languages course's assgiment using Rust language.
+
+Refer to the below links for a specific document:
+- [Assigment description](https://github.com/dapp1990/ProbabilisticProgramming/blob/master/WindowManager/assignment.pdf)
+- [Rusty code](https://github.com/dapp1990/ProbabilisticProgramming/blob/master/WindowManager/assignment.pdf)
